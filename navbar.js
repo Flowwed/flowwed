596f8e3bfbe8e7b4556becefe885a82b97d061be
Flowwed <rufat.kasumov@gmail.com>
2026-04-16 01:27:20 +0400

    Navbar Vertical 2

--- navbar.js
+++ navbar.js
@@ -13,11 +13,18 @@ export function loadNavbar() {
       padding: 20px;
       color: white;
     ">
-      <button onclick="alert('Dashboard')">Dashboard</button>
-      <br><br>
-      <button onclick="alert('Guests')">Guests</button>
-    </div>
+<button onclick="go('dashboard.html')">Dashboard</button>
+<br><br>
+<button onclick="go('Guests.html')">Guests</button>    
+</div>
   `;
 
   document.body.appendChild(nav);
 }
+
+window.go = function(page) {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("t") || "";
+
+  window.location.href = page + "?t=" + token;
+};
