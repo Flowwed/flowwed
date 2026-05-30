25b73c644093e148f449b2445189e495dd5f8fbc
Flowwed <rufat.kasumov@gmail.com>
2026-05-31 00:16:28 +0400

    Admin Password 1

---
+++ token-guard.js
@@ -0,0 +1,40 @@
+(async () => {
+
+  const token =
+    new URLSearchParams(location.search).get("t");
+
+  if (!token) return;
+
+  const SUPABASE_URL =
+    "https://octwwpatppbenqwkcqaw.supabase.co";
+
+  const SUPABASE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jdHd3cGF0cHBiZW5xd2tjcWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjYxMjYsImV4cCI6MjA3NDU0MjEyNn0.kYX1yCkx3Zl2J_qLHZYcknLnx_aXl26zB--__MzkknI";
+
+
+  // Проверяем, защищён ли токен
+
+  const protectedRes = await fetch(
+    `${SUPABASE_URL}/rest/v1/rpc/is_protected_token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${SUPABASE_KEY}`
+      },
+      body: JSON.stringify({
+        p_token: token
+      })
+    }
+  );
+
+  const isProtected = await protectedRes.json();
+
+  console.log(
+    "Protected token:",
+    token,
+    isProtected
+  );
+
+})();
